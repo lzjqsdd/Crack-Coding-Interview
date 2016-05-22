22dac6e3f308c5646f38b9757894fcf895e062c3
lzjqsdd <lzj7179@163.com>
2016-05-23 00:08:16 +0800

    struct 工程初始

--- cv/Tracking/struck/my/Config.h
+++ cv/Tracking/struck/my/Config.h
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
 //读取配置文件
+
+class Config
+{
+	public:
+		Config();
+		~Config();
+		int width; //归一化图片的大小
+		int height; //高度
+		string imgpath;
+		string resultpath;
+		int budgetSize;
+		init();
+}
